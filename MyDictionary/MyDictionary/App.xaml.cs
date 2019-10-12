034df3a7f337827e81bbbefa2abb5cbbb31b84c3
tvthuong <thuonglx123@gmail.com>
2019-10-12 12:52:41 +0700

    Add project files.

---
+++ MyDictionary/MyDictionary/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Prism.Ioc;
+using Prism.Unity;
+
+namespace MyDictionary
+{
+    public partial class App : PrismApplication
+    {
+        public App() : base() { }
+
+        protected override void OnInitialized()
+        {
+            InitializeComponent();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
